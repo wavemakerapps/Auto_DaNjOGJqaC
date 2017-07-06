e94d8c7d9c69dd0bef383b376578f9f0649a6821
root <root@61949b3d2f95>
2017-07-06 01:02:07 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_DaNjOGJqaC",
+  "homePage" : "Main",
+  "name" : "Auto_DaNjOGJqaC",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
